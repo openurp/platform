38fac82e91ef52b5bc344706a36d2b937e3ca47c
chaostone <duantihua@gmail.com>
2014-10-07 12:09:27 +0800

    add server module

--- server/src/main/scala/org/openurp/platform/server/Startup.scala
+++ server/src/main/scala/org/openurp/platform/server/Startup.scala
@@ -9,24 +9,26 @@ import org.apache.tomcat.util.descriptor.web.ContextResource
 
 object Startup {
   def main(args: Array[String]): Unit = {
-    val tomcat = new Tomcat();
-    tomcat.setPort(8080);
+    System.setProperty("cdi.profiles.active", "dev")
+    val tomcat = new Tomcat()
+    tomcat.setPort(8080)
     val base = new File(System.getProperty("java.io.tmpdir"))
     tomcat.enableNaming()
-    val resource = new ContextResource();
-    resource.setName("jdbc/dataSource");
-    resource.setAuth("Container");
-    resource.setType("javax.sql.DataSource");
-    resource.setScope("Sharable");
-    resource.setProperty("driverClassName", "org.postgresql.Driver");
-    resource.setProperty("username", "postgres");
-    resource.setProperty("password", "");
-    resource.setProperty("url", "jdbc:postgresql://localhost:5432/postgres");
-    val rootCtx = tomcat.addContext("/urp", base.getAbsolutePath());
+    val resource = new ContextResource()
+    resource.setName("jdbc/dataSource")
+    resource.setAuth("Container")
+    resource.setType("javax.sql.DataSource")
+    resource.setScope("Sharable")
+    resource.setProperty("driverClassName", "org.postgresql.Driver")
+    resource.setProperty("username", "postgres")
+    resource.setProperty("password", "")
+    resource.setProperty("url", "jdbc:postgresql://localhost:5432/postgres")
+    val rootCtx = tomcat.addContext("/urp", base.getAbsolutePath())
     rootCtx.getNamingResources().addResource(resource)
     rootCtx.addApplicationListener(classOf[BootstrapListener].getName)
 
     tomcat.start()
     tomcat.getServer().await()
+
   }
 }
