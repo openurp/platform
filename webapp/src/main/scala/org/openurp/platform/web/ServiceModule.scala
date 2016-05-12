e3bbc594384afe3893430d0a4e9c2629e764443d
xinzhou <xin.2165@gmail.com>
2016-05-12 09:00:20 +0800

    修改用户角色"info"

--- webapp/src/main/scala/org/openurp/platform/web/ServiceModule.scala
+++ webapp/src/main/scala/org/openurp/platform/web/ServiceModule.scala
@@ -3,6 +3,7 @@ package org.openurp.platform.web
 import org.beangle.commons.inject.bind.AbstractBindModule
 import org.openurp.platform.security.service.impl.{ FuncPermissionServiceImpl, MenuServiceImpl, ProfileServiceImpl }
 import org.openurp.platform.user.service.impl.{ RoleServiceImpl, UserServiceImpl }
+import org.openurp.platform.web.helper.UserDashboardHelper
 
 class ServiceModule extends AbstractBindModule {
 
@@ -13,6 +14,7 @@ class ServiceModule extends AbstractBindModule {
     bind(classOf[FuncPermissionServiceImpl])
     bind(classOf[MenuServiceImpl])
     bind(classOf[ProfileServiceImpl])
+    bind("userDashboardHelper", classOf[UserDashboardHelper])
 
   }
 }
