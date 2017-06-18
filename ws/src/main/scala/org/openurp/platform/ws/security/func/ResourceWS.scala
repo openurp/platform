b78e84639bbc382b74f0fbe666b18109d8bfe0b4
段体华 <duantihua@gmail.com>
2017-06-18 14:19:30 +0800

    fix java.time.Instant in Resource Query

--- ws/src/main/scala/org/openurp/platform/ws/security/func/ResourceWS.scala
+++ ws/src/main/scala/org/openurp/platform/ws/security/func/ResourceWS.scala
@@ -12,6 +12,7 @@ import org.beangle.commons.collection.Collections
 import org.openurp.platform.security.model.FuncPermission
 import org.openurp.platform.security.model.FuncPermission
 import org.openurp.platform.security.model.FuncPermission
+import java.time.Instant
 
 /**
  * 系统功能资源web服务
@@ -22,20 +23,22 @@ class ResourceWS(entityDao: EntityDao) extends ActionSupport with EntitySupport[
   def index(@param("app") app: String): Seq[Any] = {
     val query = OqlBuilder.from(classOf[FuncResource], "fr").where("fr.app.name=:name", app)
     get("scope") match {
-      case Some("Private") => query.where("fr.scope = :scope", Scopes.Private)
+      case Some("Private")   => query.where("fr.scope = :scope", Scopes.Private)
       case Some("Protected") => query.where("fr.scope = :scope", Scopes.Protected)
-      case Some("Public") => query.where("fr.scope = :scope", Scopes.Public)
-      case _ =>
+      case Some("Public")    => query.where("fr.scope = :scope", Scopes.Public)
+      case _                 =>
     }
     val resources = entityDao.search(query)
     val permissionQuery = OqlBuilder.from[Array[Object]](classOf[FuncPermission].getName, "fp")
-      .where("fp.resource.app.name = :appName", app).
-      where("fp.endAt is null  or fp.endAt < :now)", new java.util.Date).select("fp.resource.id,fp.role.id")
+      .where("fp.resource.app.name = :appName", app)
+      .select("fp.resource.id,fp.role.id")
+      .cacheable()
 
     val permissions = Collections.newMap[Number, collection.mutable.Set[Number]]
     entityDao.search(permissionQuery) foreach { p =>
       permissions.getOrElseUpdate(p(0).asInstanceOf[Number], new collection.mutable.HashSet[Number]) += p(1).asInstanceOf[Number]
     }
+
     resources map { r =>
       val p = new Properties(r, "id", "name", "title", "scope")
       p.put("roles", permissions.getOrElse(r.id, Set.empty).toArray)
@@ -51,7 +54,8 @@ class ResourceWS(entityDao: EntityDao) extends ActionSupport with EntitySupport[
     if (!resources.isEmpty) {
       val roleQuery = OqlBuilder.from[Integer](classOf[FuncPermission].getName, "fp")
         .where("fp.resource.app.name = :appName", app).where("fp.resource.name =:resourceName", name)
-        .where("fp.endAt is null  or fp.endAt < :now)", new java.util.Date).select("fp.role.id")
+        .select("fp.role.id")
+        .cacheable()
       val p = new Properties(resources.head, "id", "name", "title", "scope")
       p.put("roles", entityDao.search(roleQuery).toArray)
       p
@@ -63,7 +67,9 @@ class ResourceWS(entityDao: EntityDao) extends ActionSupport with EntitySupport[
   @response
   @mapping("public")
   def pub(@param("app") app: String): Seq[Any] = {
-    val query = OqlBuilder.from(classOf[FuncResource], "fr").where("fr.app.name=:name", app).where("fr.scope=:scope", Scopes.Public)
+    val query = OqlBuilder.from(classOf[FuncResource], "fr").where("fr.app.name=:name", app)
+      .where("fr.scope=:scope", Scopes.Public)
+      .cacheable()
     entityDao.search(query)
   }
 
