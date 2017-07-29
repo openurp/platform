e3e1cf0599a16c9841f6c8e39e6d66a4f1bed4d6
chaostone <duantihua@gmail.com>
2017-07-29 13:12:49 +0800

    add SessionInfo mapping

---
+++ core/ids/src/main/scala/org/openurp/platform/ids/session/model/SessionInfo.scala
@@ -0,0 +1,16 @@
+package org.openurp.platform.ids.session.model
+
+import org.beangle.data.model.StringId
+import java.time.Instant
+import java.time.Duration
+
+class SessionInfo extends StringId {
+  var principal: String = _
+  var description: Option[String] = _
+  var ip: Option[String] = _
+  var agent: Option[String] = _
+  var os: Option[String] = _
+  var loginAt: Instant = _
+  var timeout: Duration = _
+  var lastAccessAt: Instant = _
+}
