893bc9e73d23bc49a82221dd7a74f826d2a6fb52
chaostone <duantihua@gmail.com>
2017-08-24 16:04:26 +0800

    remove default serializer binding

--- core/ids/src/main/scala/org/openurp/platform/ids/cas/Modules.scala
+++ core/ids/src/main/scala/org/openurp/platform/ids/cas/Modules.scala
@@ -7,7 +7,6 @@ import org.beangle.cache.redis.JedisPoolFactory
 import org.beangle.cdi.PropertySource
 import org.beangle.cdi.bind.BindModule
 import org.beangle.commons.collection.Collections
-import org.beangle.commons.io.DefaultBinarySerializer
 import org.beangle.commons.lang.Strings
 import org.beangle.data.jdbc.ds.DataSourceFactory
 import org.beangle.ids.cas.id.impl.DefaultServiceTicketIdGenerator
@@ -67,7 +66,6 @@ class DefaultModule extends BindModule with PropertySource {
 class TicketModule extends BindModule {
   override def binding() {
     bind("jedis.Factory", classOf[JedisPoolFactory]).constructor(Map("host" -> $("redis.host"), "port" -> $("redis.port")))
-    bind("serializer.default", DefaultBinarySerializer)
     bind(classOf[DefaultTicketCacheService]).constructor(ref("jedis.Factory"))
     bind(classOf[DefaultTicketRegistry])
     bind(classOf[DefaultServiceTicketIdGenerator])
