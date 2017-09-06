ddcc78003cabaf6572ec7d3f94d1b6e47c06798d
chaostone <duantihua@gmail.com>
2017-09-06 12:08:51 +0800

    update license header

--- cas/src/main/scala/org/openurp/platform/cas/service/DaoAccountStore.scala
+++ cas/src/main/scala/org/openurp/platform/cas/service/DaoAccountStore.scala
@@ -1,3 +1,21 @@
+/*
+ * Beangle, Agile Development Scaffold and Toolkit
+ *
+ * Copyright (c) 2005-2017, Beangle Software.
+ *
+ * Beangle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beangle is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Beangle.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openurp.platform.cas.service
 
 import org.beangle.commons.collection.Collections
@@ -5,45 +23,57 @@ import org.beangle.data.dao.{ EntityDao, OqlBuilder }
 import org.beangle.security.authc.{ Account, AccountStore, DefaultAccount }
 import org.openurp.platform.user.model.{ RoleMember, UserProfile }
 import org.openurp.platform.user.service.UserService
+import org.beangle.data.hibernate.spring.SessionUtils
+import org.beangle.data.hibernate.HibernateEntityDao
+import org.hibernate.SessionFactory
 
-class DaoAccountStore(userService: UserService, entityDao: EntityDao) extends AccountStore {
+class DaoAccountStore(userService: UserService, entityDao: EntityDao, sf: SessionFactory) extends AccountStore {
 
   def load(principal: Any): Option[Account] = {
+    SessionUtils.enableBinding(sf)
+    try {
+      val rs = userService.get(principal.toString) match {
+        case Some(user) =>
+          val account = new DefaultAccount(user.code, user.name)
+          account.accountExpired = user.accountExpired
+          account.accountLocked = user.locked
+          account.credentialExpired = user.credentialExpired
+          account.disabled = !user.enabled
+          account.details += "category" -> user.category.id.toString
 
-    userService.get(principal.toString) match {
-      case Some(user) =>
-        val account = new DefaultAccount(user.code, user.name)
-        account.accountExpired = user.accountExpired
-        account.accountLocked = user.locked
-        account.credentialExpired = user.credentialExpired
-        account.disabled = !user.enabled
-        account.details += "category" -> user.category.id.toString
+          val query = OqlBuilder.from[Int](classOf[RoleMember].getName, "rm")
+            .where("rm.user=:user and rm.member=true", user)
+            .select("rm.role.id")
+          val rs = entityDao.search(query)
+          account.authorities = rs.mkString(",")
 
-        val query = OqlBuilder.from[Int](classOf[RoleMember].getName, "rm")
-          .where("rm.user=:user and rm.member=true", user)
-          .select("rm.role.id")
-        val rs = entityDao.search(query)
-        account.authorities = rs.mkString(",")
+          val upQuery = OqlBuilder.from(classOf[UserProfile], "up").where("up.user=:user", user)
+          val ups = entityDao.search(upQuery)
 
-        val ups = entityDao.search(OqlBuilder.from(classOf[UserProfile], "up").where("up.user=:user", user))
-        if (!ups.isEmpty) {
-          val domainUps = ups.groupBy(up => up.domain)
-          domainUps foreach {
-            case (domain, dups) =>
-              val str = new StringBuilder
-              str += '['
-              val profiles = Collections.newBuffer[String]
-              dups foreach { up =>
-                val ps = up.properties map (e => e._1.name + ":" + e._2)
-                profiles += "{" + ps.mkString(",") + "}"
-              }
-              str += ']'
-              account.details += "profiles_" + domain.name -> str.toString
+          if (!ups.isEmpty) {
+            val domainUps = ups.groupBy(up => up.domain)
+            domainUps foreach {
+              case (domain, dups) =>
+                val str = new StringBuilder
+                str += '['
+                val profiles = Collections.newBuffer[String]
+                dups foreach { up =>
+                  val ps = up.properties map (e => e._1.name + ":'" + e._2 + "'")
+                  profiles += "{" + ps.mkString(",") + "}"
+                }
+                str ++= profiles.mkString(",")
+                str += ']'
+                account.details += "profiles_" + domain.name -> str.toString
+            }
           }
-        }
 
-        Some(account)
-      case None => None
+          Some(account)
+        case None => None
+      }
+      rs
+    } finally {
+      SessionUtils.disableBinding(sf)
+      SessionUtils.closeSession(sf)
     }
   }
 }
