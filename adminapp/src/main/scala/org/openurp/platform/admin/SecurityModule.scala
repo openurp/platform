007872ea122a7fa6d7d24f34f0ef08a103ff0ec5
段体华 <duantihua@gmail.com>
2018-08-29 18:03:46 +0800

    refact app package

---
+++ adminapp/src/main/scala/org/openurp/platform/admin/SecurityModule.scala
@@ -0,0 +1,45 @@
+/*
+ * OpenURP, Agile University Resource Planning Solution.
+ *
+ * Copyright © 2005, The OpenURP Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openurp.platform.admin
+
+import org.beangle.cdi.bind.BindModule
+import org.openurp.platform.admin.action.IndexAction
+import org.openurp.platform.admin.action.bulletin.{ DocAction, NewsAction, NoticeAction }
+import org.openurp.platform.admin.action.security.{ DashboardAction, DataPermissionAction, DataResourceAction, FuncResourceAction, MenuAction, PermissionAction }
+import org.openurp.platform.admin.action.user.{ AccountAction, AvatarAction, DimensionAction, ProfileAction, RoleAction, UserAction }
+import org.openurp.platform.admin.helper.UserDashboardHelper
+
+class SecurityModule extends BindModule {
+
+  protected override def binding() {
+    bind(classOf[AccountAction], classOf[DashboardAction], classOf[AvatarAction])
+    bind(classOf[DimensionAction], classOf[PermissionAction], classOf[UserAction],
+      classOf[RoleAction], classOf[ProfileAction])
+
+    bind(classOf[DocAction], classOf[NoticeAction], classOf[NewsAction])
+
+    bind(classOf[FuncResourceAction], classOf[MenuAction])
+    bind(classOf[DataPermissionAction])
+    bind(classOf[DataResourceAction])
+
+    bind(classOf[IndexAction])
+
+    bind("userDashboardHelper", classOf[UserDashboardHelper])
+  }
+}
---
+++ adminapp/src/main/scala/org/openurp/platform/admin/action/user/DimensionAction.scala
@@ -0,0 +1,49 @@
+/*
+ * OpenURP, Agile University Resource Planning Solution.
+ *
+ * Copyright © 2005, The OpenURP Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openurp.platform.admin.action.user
+
+import org.beangle.webmvc.api.annotation.ignore
+import org.beangle.webmvc.api.view.View
+import org.beangle.webmvc.entity.action.RestfulAction
+import org.openurp.platform.user.model.Dimension
+import org.openurp.platform.config.model.Domain
+
+/**
+ * 数据限制域元信息配置类
+ *
+ * @author chaostone
+ */
+class DimensionAction extends RestfulAction[Dimension] {
+
+  override def editSetting(dimension: Dimension): Unit = {
+    val domains = entityDao.getAll(classOf[Domain]).toBuffer -- dimension.domains
+    put("domains", domains)
+  }
+  protected override def saveAndRedirect(field: Dimension): View = {
+    if (entityDao.duplicate(classOf[Dimension], field.id, "name", field.name)) {
+      addError("名称重复")
+      return forward(to(this, "edit"))
+    }
+    field.domains = new collection.mutable.ListBuffer[Domain]
+    val domainId2nd = getAll("domainId2nd", classOf[Int])
+    field.domains ++= entityDao.find(classOf[Domain], domainId2nd)
+    entityDao.saveOrUpdate(field)
+    redirect("search", "info.save.success")
+  }
+}
